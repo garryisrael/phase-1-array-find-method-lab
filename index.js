84d121e910b52a02aabb0eef3835e78d6a04511b
Garry <garryisrael02@gmail.com>
2023-04-30 12:14:32 -0400

    Uses find method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(record){
+  for(let game of record){
+    if(game.result === "W"){
+      return game.year;
+    }
+  }
+}
+
+const outcome = record.find(superbowlWin);
+console.log(outcome);
+
+
+
+
+
